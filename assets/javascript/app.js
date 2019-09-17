75ab5a9de1a33e7859cc580c0a006bdb31ae0daf
AARON M <asmarcks@gmail.com>
2019-09-16 21:16:24 -0500

    submit button,,,,result values,,,es no functionita

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,198 @@
-// Initial Values
-var counter = 45;
 
-var currentQuestion = 0;
 
-var score = 0;
 
-var timer;
 
-var quizQuestions = [
+$('#beginbutton').on('click', function () {
+    game.start();
+    var audioElement = document.createElement("audio");
+    audioElement.setAttribute("src", "assets/images/bravesir.wav");
+    audioElement.play();
+    $('#rabbit').hide();
+})
 
-    {
+$(document).on('click','#end',function(){
 
-        question: 'How many Planet Earths are there?',
-        choices: ['1,2,3,4'],
-        correctAnswer: '1'
+    game.done();
+
+})
+
+var questions = [{
+
+    question: 'What is your name?',
+    answers: ['Bill', 'Montgomery', 'Arthur, King of the Britians', 'Toby'],
+    correctAnswer: 'Arthur, King of the Britians'
+
+}, {
+
+    question: 'What is your quest?',
+    answers: ['To seek the ark of the covenant', 'To make peace throughout the land', 'To seek the holy grail', 'None of these'],
+    correctAnswer: ''
+
+}, {
+
+    question: 'What is your favorite color?',
+    answers: ['black', 'clear', 'pink', 'blue...no yellow!'],
+    correctAnswer: 'blue...no yellow!'
+
+}, {
+
+    question: 'In the "bring out your dead scene" an old woman can be seen beating what against the side of her house?',
+    answers: ['her son', 'a cat', 'a rug', 'a coconut'],
+    correctAnswer: 'a cat'
+
+}, {
+
+    question: 'The knights who say "NI" are also keepers of which other secret word?',
+    answers: ['Noo', 'Nigh', 'Nee-Wom', 'Icky'],
+    correctAnswer: 'Nee-Wom'
+
+}, {
+
+    question: 'The knights of "NI" also wanted Arthur to cut down the mightiest tree in the forrest with what?',
+    answers: ['a cat', 'a knife', 'the holy grail', 'a herring'],
+    correctAnswer: 'a herring'
+
+}, {
+
+    question: 'What is the air speed velocity of an un-laiden swallow?',
+    answers: ['9.8 meters/sec/sec', 'African or European swallow?', '40mph', 'I do not know that'],
+    correctAnswer: 'African or European swallow?'
+
+}, {
+
+    question: 'What was the name of the fire-shooting wizard?',
+    answers: ['Bill', 'Tim', 'Arthur', 'Raistlin', 'Toby'],
+    correctAnswer: 'Tim'
+
+
+
+}]
+
+var game = {
+
+    correct: 0,
+    incorrect: 0,
+    counter: 25,
+    countdown: function () {
+        game.counter--;
+        $('#counter').html(game.counter);
+        if (game.counter == 0) {
+            console.log('Times up!');
+            clearInterval(timer)
+
+
+
+
+
+            $('#subwrapper h2').remove();
+            $('#subwrapper').html("<h2>Results:</h2>");
+            $('#subwrapper').append("<h3>Correct Answers: "+game.correct+" </h3>");
+            $('#subwrapper').append("<h3>Incorrect Answers: "+game.incorrect+" </h3>");
+            $('#subwrapper').append("<h3>Unanswered:"+(questions.length-(game.incorrect+game.correct))+"</h3>");
+            game.done();
+
+
+
+        }
+
+
     },
 
-    {  
-        question: 'What is a Cat?',
-        choices: ['a dog,a country,a feline, a stick of gum'],
-        correctAnswer: 'a feline'
+    start: function () {
+        timer = setInterval(game.countdown, 1000);
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">25</span> Seconds</h2>');
+        $('#beginbutton').remove();
+        for (var i = 0; i < questions.length; i++) {
+            $('#subwrapper').append('<h2>' + questions[i].question + '<h2>');
+            for (var j = 0; j < questions[i].answers.length; j++) {
+                $('#subwrapper').append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+            }
+
+        }
+        $('#subwrapper').append("<button id='end'>Submit Answers</button>");
     },
+    done: function () {
 
-    {
 
-        question: 'What sound does a doggy make?',
-        choices: ['gickoreee gee!,fa-wamp!,Woof!, hello!'],
-        correctAnswer: 'Woof!'
 
-    },
+        $.each($('input[name="question-0]":checked'), function () {
+            if ($(this).val() == questions[0].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]":checked'), function () {
+            if ($(this).val() == questions[1].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-2]":checked'), function () {
+            if ($(this).val() == questions[2].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-3]":checked'), function () {
+            if ($(this).val() == questions[3].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-4]":checked'), function () {
+            if ($(this).val() == questions[4].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-5]":checked'), function () {
+            if ($(this).val() == questions[5].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-6]":checked'), function () {
+            if ($(this).val() == questions[6].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-7]":checked'), function () {
+            if ($(this).val() == questions[7].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
 
-];
-//Funktion that starts the timer to count down from 45
-function countDown(){
+       
 
-    counter--;
-
-    $('#time').html('Time' + counter);
+        }
 
 }
-// console.log(quizQuestions)
-
-//Display the question and the choices in the Browser
-function loadQuestion(){
-    counter = 45;
-    timer = setInterval(countDown, 1000);
-
-    var question = quizQuestions[currentQuestion].question;
-    var choices = quizQuestions[currentQuestion].choices;
-
-    $('#time').html('Time:' + counter);
-    $('#game').html('<h5>' + question + '</h5>'); 
-
-
-}
 
 
 
-    loadQuestion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
